0c987942d7094af17bfea8a143f4db89f2206ca1
Aniket Singh <aniketpsingh2004@gmail.com>
2026-05-08 13:29:31 +0530

    backend fix

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/test', testRoutes);
